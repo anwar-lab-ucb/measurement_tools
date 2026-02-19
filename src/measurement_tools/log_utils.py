8347cc6c4c6fa7319fcb1f6467793f76448c438b
Alec Vercruysse <vercruysse.alec@gmail.com>
2026-02-19 13:38:32 -0800

    reconfigure create_logger to set global logging settings by default

--- src/measurement_tools/log_utils.py
+++ src/measurement_tools/log_utils.py
@@ -1,18 +1,29 @@
 import logging
 import sys
+from pathlib import Path
 
 
 def create_logger(name=None, fname=None, quiet=False, level=logging.DEBUG):
-    """If `fname` is None, just prints to stdout"""
-    if name == None:
-        name = __name__
+    """Configure a logger with standardized formatting.
+
+    If `name` is None, configures the root logger (useful for application
+    entry points - child loggers will inherit handlers).
+    If `fname` is None, just prints to stdout.
+    If `quiet` is True, suppresses stdout output.
+    """
     logger = logging.getLogger(name)
+
+    # a logger of this name might have previously been registered
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
     logger.setLevel(level)
     formatter = logging.Formatter(
         "%(levelname)-8s %(name)s:%(filename)s:%(lineno)d (@ %(asctime)s) %(message)s"
     )
 
     if fname:
+        Path(fname).parent.mkdir(parents=True, exist_ok=True)
         file_handler = logging.FileHandler(fname, encoding="utf-8")
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
